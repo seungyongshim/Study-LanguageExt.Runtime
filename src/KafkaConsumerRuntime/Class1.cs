2c5e1c38e530f923f04196ed2086f538b39668ee
SeungYongShim <syshim@me.com>
2021-08-12 19:06:19 +0900

    234234

--- src/KafkaConsumerRuntime/Class1.cs
+++ src/KafkaConsumerRuntime/Class1.cs
@@ -36,19 +36,20 @@ namespace KafkaConsumerRuntime
 
         public static Eff<RT, ConsumeResult<string, string>> consume(IConsumer<string, string> consumer) =>
             from msg in Eff<RT, ConsumeResult<string, string>>(rt => consumer.Consume(rt.CancellationToken))
-            from __1 in guard(msg == null, Error.New(0, "ConsumeResult is null"))
-            from __2 in guard(msg.IsPartitionEOF, Error.New(0, "ConsumeResult is PartitionEOF"))
+            from __1 in guardnot(msg is null, Error.New(0, "ConsumeResult is null"))
+            from __2 in guardnot(msg.IsPartitionEOF, Error.New(0, "ConsumeResult is PartitionEOF"))
             select msg;
 
         public static Eff<RT, Activity> activity(ConsumeResult<string, string> msg) =>
-            Eff<RT, Activity>(rt => new ActivitySource("Aums", "").StartActivity("kafka"));
+            Eff<RT, Activity>(rt => new ActivitySource("Aums").StartActivity("kafka"));
 
         public static Eff<RT, object> parse(ConsumeResult<string, string> msg) =>
-            Eff<RT, object>(rt => "1");
+            Eff<RT, object>(rt => msg.Message.Value);
 
         public static Eff<RT, Unit> commit(ConsumeResult<string, string> msg) =>
             Eff<RT, Unit>(rt => unit);
-
+        public static Eff<RT, Unit> subscribe(IConsumer<string, string> consumer, string topic) => 
+            Eff<RT, Unit>(rt => { consumer.Subscribe(topic); return unit; });
     }
 
     public static class Controller<RT> where
@@ -56,12 +57,14 @@ namespace KafkaConsumerRuntime
     {
         public static Aff<RT, Unit> SendMail(ConsumerConfig config, Func<object, Aff<Unit>> func) =>
             use(KafkaConsumer<RT>.build(config), c =>
-                repeat(from m in KafkaConsumer<RT>.consume(c)
-                       from _ in use(KafkaConsumer<RT>.activity(m), c =>
-                                     from r in KafkaConsumer<RT>.parse(m).ToAff()
-                                     from _1 in func(r)
-                                     from _2 in KafkaConsumer<RT>.commit(m).ToAff()
-                                     select unit)
-                       select unit));
+                from s in KafkaConsumer<RT>.subscribe(c, "aums.send-mail").ToAff()
+                from _ in repeat((from m  in KafkaConsumer<RT>.consume(c)
+                                  from _1 in use(KafkaConsumer<RT>.activity(m), a =>
+                                                 from r in KafkaConsumer<RT>.parse(m).ToAff()
+                                                 from _1 in func(r)
+                                                 from _2 in KafkaConsumer<RT>.commit(m).ToAff()
+                                                 select unit) 
+                                  select unit)|@catch(0, unit))
+                select unit);
     }
 }
